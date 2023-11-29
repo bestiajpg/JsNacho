ce3e74e217f19a11b97539a20ff9ad562afec328
bestiajpg <152210219+bestiajpg@users.noreply.github.com>
2023-11-29 18:19:08 -0300

    Subiendo la versión 1 de nuestro código

---
+++ NachoJS/Juego/app1.js
@@ -0,0 +1,13 @@
+const letterContainer= document.getElementById("letter-container");
+const optionsContainer = document.getElementById ("options-container");
+const userInputSection = document.getElementById("user-input-section");
+const newGameContainer = document.getElementById("new-game-container");
+const newGameButton = document.getElementById("new-game-button");
+const canvas = document.getElementById("canvas");
+const resultText =document.getElementById("result-text");
+
+
+
+
+//camelCase : la primera palabra toda en minúscula y a partir de la segunda
+//palabra en adelante se pone la primera letra en Mayúscula
